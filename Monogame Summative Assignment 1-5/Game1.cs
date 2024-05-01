dd38a7a0f9033e33a980acd40a99be1c4042cd4d
Chickenmist <wpimlatt@gmail.com>
2024-05-01 14:40:51 -0400

    added title screen background and made same changes to the rectange properties

--- Monogame Summative Assignment 1-5/Game1.cs
+++ Monogame Summative Assignment 1-5/Game1.cs
@@ -3,17 +3,18 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace Monogame_Summative_Assignment_1_5
-{
+{ 
+    enum Screen
+    {
+        Title
+    }
+    
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        enum Screen
-        {
-            Intro
-
-        }
+       
 
         Screen screen;
 
@@ -21,10 +22,13 @@ namespace Monogame_Summative_Assignment_1_5
 
         MouseState mouseState;
 
+        Texture2D titleScreenBackground;
+
+        Rectangle tailsRect;
         Texture2D tailsTexture; //Image size is 80x80 pixels
         //Tails and Sonic textures have a height difference of 32 pixels. This is important for keeping them on level ground
         Texture2D sonicTexture; //Image size is 80x112 pixels
-
+        Rectangle sonicRect;
         Texture2D eggmanTexture; //Image size is 100x141 pixels
 
         Texture2D ringTexture; //Image size is 20x20 pixels
@@ -47,11 +51,16 @@ namespace Monogame_Summative_Assignment_1_5
             _graphics.PreferredBackBufferHeight = 600;
             _graphics.ApplyChanges();
 
-            sonicDamagedRect = new Rectangle(40, 40, 280, 100);
+            sonicDamagedRect = new Rectangle(40, 40, 150, 70);
 
             seconds = 0;
 
+            screen = Screen.Title;
+
             base.Initialize();
+
+            sonicRect = new Rectangle(185, 100, sonicTexture.Width, sonicTexture.Height);
+            tailsRect = new Rectangle(100, 132, tailsTexture.Width, tailsTexture.Height);
         }
 
         protected override void LoadContent()
@@ -59,11 +68,14 @@ namespace Monogame_Summative_Assignment_1_5
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-
-            tailsTexture = Content.Load<Texture2D>("tailsSprite");
+            
             sonicTexture = Content.Load<Texture2D>("sonicSprite");
+            tailsTexture = Content.Load<Texture2D>("tailsSprite");
+            
             ringTexture = Content.Load<Texture2D>("ringSprite");
             sonicDamagedTexture = Content.Load<Texture2D>("sonicDamagedSprite");
+
+            titleScreenBackground = Content.Load<Texture2D>("sonicAnimationTitle");
         }
 
         protected override void Update(GameTime gameTime)
@@ -90,8 +102,10 @@ namespace Monogame_Summative_Assignment_1_5
 
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(tailsTexture, new Vector2(100, 132), Color.White);
-            _spriteBatch.Draw(sonicTexture, new Vector2(185, 100), Color.White);
+
+
+            _spriteBatch.Draw(tailsTexture, tailsRect, Color.White);
+            _spriteBatch.Draw(sonicTexture, sonicRect, Color.White);
             _spriteBatch.Draw(ringTexture, new Vector2(205, 120), Color.White);
             _spriteBatch.Draw(sonicDamagedTexture, sonicDamagedRect, Color.White);
 
